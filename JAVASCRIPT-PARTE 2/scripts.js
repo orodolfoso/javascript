2a5a16b9a56fedb53f92cc6b6f6255f0b9194dcb
orodolfoso <orodolfoso.dev@gmail.com>
2025-02-04 11:21:30 -0300

    Seletores

--- JAVASCRIPT-PARTE 2/scripts.js
+++
@@ -1,7 +0,0 @@
-const input = document.getElementById("main-input")
-
-console.log(input)
-
-const elements = document.getElementsByClassName("paragraph-js")
-
-console.log(elements)
---
+++ JAVASCRIPT-PARTE 2/seletores.js
@@ -0,0 +1,39 @@
+// getElementById - para id
+
+const input = document.getElementById("main-input")
+
+console.log(input)
+
+// getElementByClassName - para classes
+
+const elements = document.getElementsByClassName("paragraph-js")
+
+console.log(elements)
+
+// getElementByTagName - para Tags
+
+const paragraph = document.getElementsByTagName("p")
+
+console.log(paragraph)
+
+// document.getElementsByName - Trás todos os elementos com  esse NAME
+
+const names = document.getElementsByName("nome-completo")
+
+console.log(names)
+
+// querySelector - Trás um elemento, o PRIMEIRO que encontrar
+
+const first = document.querySelector("button.main-button")
+
+console.log(first)
+
+// querySelectorAll - Trás todos os elementos que encontar
+
+const all = document.querySelectorAll("p")
+
+
+
+console.log(all)
+
+
