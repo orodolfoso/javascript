dd73d96e4cab2e4b668e8673e52a7431d6e396ff
orodolfoso <orodolfoso.dev@gmail.com>
2025-02-20 21:11:52 -0300

    Método Math concluído

---
+++ JAVASCRIPT-PARTE 2/math.js
@@ -0,0 +1,27 @@
+/* Math - Matemática
+ - pow (2,2) / potência / 2² = 4
+ - sqrt (25,2) / raiz / √25
+ - PI  / 3,14....
+ - ceil / teto -> arredondar para cima
+ - floor / chão -. arredondar para baixo
+
+ - random () / número aleatório entre 0 e 1 
+*/ 
+ 
+// const result = Math.pow(2,3)
+// console.log(result)
+
+// const result = Math.sqrt(25,3)
+// console.log(result)
+
+// const result = Math.PI  
+// console.log(result)
+
+// const result = Math.ceil(3.14)  
+// console.log(result)
+
+// const result = Math.floor(3.14)  
+// console.log(result)
+
+const result = Math.random()
+console.log(result)
