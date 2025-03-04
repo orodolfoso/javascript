cf4cc6fa6f8952a9e3f28b9b4d99db02fc6c92c3
orodolfoso <orodolfoso.dev@gmail.com>
2025-03-04 17:02:00 -0300

    Funções e argumentos

---
+++ JAVASCRIPT-PARTE 4/Argumentos-parametros.js
@@ -0,0 +1,37 @@
+/*
+
+Functions / Funções
+
+Funções podem receber argumentos ou parâmetros.
+
+*/
+
+
+// function sayMyName(name) {
+// console.log(`O nome é ${name}`)
+// }
+
+// sayMyName('Lucas')
+// sayMyName('Rodolfo')
+// sayMyName('Renata')
+// sayMyName('Maria')
+// sayMyName('José')
+
+
+// function sum(value, value2) {
+//     console.log(value + value2) 
+
+    
+// }
+
+// sum(20, 30)
+
+const a = 15
+const b = 3
+function sum1(a = 10, b = 3) {
+    console.log(a - b) 
+}
+
+sum1()
+
+
