2e97f5df43f5aa29509e4f224f37c8fa71794991
orodolfoso <orodolfoso.dev@gmail.com>
2025-03-11 20:45:58 -0300

    getElementById

---
+++ JAVASCRIPT-PARTE 2/document.js
@@ -0,0 +1,17 @@
+/*
+document -> HTML
+
+getElementById -> Trás um elemento pelo ID
+getElementClassName -> Trás TODOS os elementos com essa classe
+getElementByTagName -> Trás TODOS os elementos com essa TAG
+getElementByName -> Trás TODOS os elementos com esse NAME
+
+querySelector -> Trás UM elemento, o PRIMEIRO que encontar
+querySelectorAll -> Trás TODOS os elementos que encontrar
+
+
+*/
+
+const input = document.querySelector("main-input")
+
+console.log(input)
