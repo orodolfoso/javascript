8cc6ef898a5cac2d9df88803d78a6177e3c04945
orodolfoso <orodolfoso.dev@gmail.com>
2025-03-12 10:06:46 -0300

    Alterando dados no JS

--- JAVASCRIPT-PARTE 2/document.js
+++ JAVASCRIPT-PARTE 2/document.js
@@ -12,6 +12,42 @@ querySelectorAll -> Trás TODOS os elementos que encontrar
 
 */
 
-const input = document.querySelector("main-input")
+// const element = document.querySelector("img")
 
-console.log(input)
+// console.log(element.src)
+
+// element.src = "./dado1.png"
+
+// const element = document.querySelector(".paragraph-js")
+
+// element.value = 1985
+// element.placeholder = "Uma nova mensagem"
+// console.log(element)
+
+// element.innerHTML = "Coloquei uma nova frase nestes paragrfos!"
+
+// ALTERANDO TEXTOS E HTML NO JS
+
+// element.innerHTML = "Cheguei <b>Brasil!</b>"
+// console.log(element.innerContent) //SÓ HTML
+// console.log(element.innerText) // LEVA EM CONTA O CSS
+// console.log(element.innerHTML) // TRÁS TUDO - E PERMITE ADICIONAR HTML
+
+
+
+// console.log(element)
+// console.log(element)
+// console.log(element)
+
+
+// element.value = 2013   
+// console.log(element.value)
+
+
+// ALTERANDO CSS(estilos) NO JS
+
+
+const button = document.querySelector("h1")
+
+button.style.fontFamily= "Arial"
+button.style.fontSize= "80px"
