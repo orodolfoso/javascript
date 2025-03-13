a811909fd6ff7f9a2817cd7786bac5617dff0504
orodolfoso <orodolfoso.dev@gmail.com>
2025-03-13 17:53:44 -0300

    AddEventListener

---
+++ JAVASCRIPT-PARTE 2/evento.js
@@ -0,0 +1,28 @@
+
+
+
+// const input = document.querySelector('#main-input')
+
+
+
+// function clickButton() {
+//     addEventListener.paragraph
+// }
+
+
+// function digiteiNoInput() {
+//     paragraph.innerHTML = ''
+// }
+
+const input = document.querySelector('#main-input')
+const select = document.querySelector('select')
+const button = document.querySelector('.main-button')
+
+
+
+function TroqueiValor(event) {
+    console.log(event)
+}
+
+select.addEventListener('keypress', TroqueiValor)
+ 
